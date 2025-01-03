fe0ff349ee02b7f673112970ccf6bc4cc6bbc7b4
Gideon Jacob <gideonjacob.in@gmail.com>
2025-01-04 00:02:28 +0530

    Updated Not Found Page

--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -1,10 +1,12 @@
+import { PiSmileySadDuotone as NotFoundIcon } from "react-icons/pi";
 import NavbarWrapper from '../NavbarWrapper'
 import './index.css'
 
 const NotFound = () => {
   return (
-    <NavbarWrapper>
-      <h1>404 Not Found</h1>
+    <NavbarWrapper className="not-found-countainer">
+      <NotFoundIcon className="not-found-icon" />
+      <h1 className="not-found-heading">Page Not Found</h1>
     </NavbarWrapper>
   )
 }
