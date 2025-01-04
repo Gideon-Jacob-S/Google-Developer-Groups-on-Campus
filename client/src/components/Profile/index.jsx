8a25a2ed4899f7636a91bf6b1b0821f0143b37ac
Gideon Jacob <gideonjacob.in@gmail.com>
2025-01-04 18:00:11 +0530

    Updated docker files to run both server and client services. Migrated client side code to vite for faster developments process

---
+++ client/src/components/Profile/index.jsx
@@ -0,0 +1,24 @@
+import NavbarWrapper from '../NavbarWrapper'
+import './index.css'
+
+const Profile = props => {
+  // const {state} = this.props.location
+  // const {isLoginSuccess, isUnauthorized} = state ? state : {}
+
+  return (
+    <NavbarWrapper>
+      <div className="profile-secion">
+        <h1>Your Profile</h1>
+        <p>
+          Welcome to the Google Developer Groups on Campus! We are a community
+          of developers and tech enthusiasts who come together to learn, share,
+          and grow. Our mission is to foster a collaborative environment where
+          students can enhance their skills, network with industry
+          professionals, and contribute to open source projects.
+        </p>
+      </div>
+    </NavbarWrapper>
+  )
+}
+
+export default Profile
